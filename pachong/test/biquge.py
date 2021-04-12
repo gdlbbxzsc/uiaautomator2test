f1c60c7811b00783b3b18a4c31fd2346d05f8b8e
Gdl <gdlbbxzsc@163.com>
2021-04-12 17:21:55 +0800

    s

--- pachong/test/biquge.py
+++ pachong/test/biquge.py
@@ -2,93 +2,56 @@ import requests
 import parsel
 import csv
 import threading
+import pprint
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 }
 
+mulus = []
 
-def xiaoshuo_list():
+
+def xiaoshuo_mulu():
     url = 'http://www.dvdspring.com/b/46834/'
     response = requests.get(url=url, headers=headers)
 
     selector = parsel.Selector(response.text)
 
     dd_list = selector.xpath('//*[@id="list"]/dl/dd')
-    print(dd_list)
-    results = []
+    global mulus
+    mulus = []
     for dd_item in dd_list:
+        # 通过xpath获取链接
+        href_item = dd_item.xpath('.//a/@href').get()
+        # pprint.pprint(href_item)
+        # 通过css
+        # href_item1 = dd_item.css('a::attr(href)').get()
+        # pprint.pprint(href_item1)
 
-        href_item = dd_item.css('a@href')
-        results.append(href_item)
-        print(href_item)
+        mulus.append(href_item)
+
         pass
-    list(set(results)).sort()
-    print(results)
+    # 通过set进行去重并排序
+    list(set(mulus)).sort()
+    pprint.pprint(mulus)
     pass
 
 
-def main(page):
-    url = 'http://www.dvdspring.com/b/46834/'
-
-    params = {
-        'compkind': '',
-        'dqs': '',
-        'pubTime': '',
-        'pageSize': '40',
-        'salary': '',
-        'compTag': '',
-        'sortFlag': '',
-        'degradeFlag': '0',
-        'compIds': '',
-        'subIndustry': '',
-        'jobKind': '',
-        'industries': '',
-        'compscale': '',
-        'key': 'python',
-        'siTag': 'I-7rQ0e90mv8a37po7dV3Q~fA9rXquZc5IkJpXC-Ycixw',
-        'd_sfrom': 'search_fp',
-        'd_ckId': 'cd74f9fdbdb63c6d462bad39feddc7f1',
-        'd_curPage': '2',
-        'd_pageSize': '40',
-        'd_headId': 'cd74f9fdbdb63c6d462bad39feddc7f1',
-        'curPage': page,
-    }
-
-    response = requests.get(url=url, params=params, headers=headers)
+def xiaoshuo_neirong(page):
+    url = r'http://www.dvdspring.com{}'.format(mulus[page])
+    pprint.pprint(str(page)+" "+url)
+    response = requests.get(url=url, headers=headers)
 
     selector = parsel.Selector(response.text)
 
-    lis = selector.css('div.job-content div:nth-child(1) ul li')
-    for li in lis:
-        title = li.css('.job-info h3 a::text').get().strip()
-        money = li.css('.condition span.text-warning::text').get()
-        city = li.css('.condition .area::text').get()
-        edu = li.css('.condition .edu::text').get()
-        experience = li.css('.condition span:nth-child(4)::text').get()
-        company = li.css('.company-name a::text').get()
-        financing = li.css('.field-financing span::text').get()
-        temptation_list = li.css('p.temptation.clearfix span::text').getall()
-        temptation_str = '|'.join(temptation_list)
-        release_time = li.css('p.time-info.clearfix time::text').get()
-        feedback_time = li.css('p.time-info.clearfix span::text').get()
-        dit = {
-            '标题': title,
-            '薪资': money,
-            '城市': city,
-            '学历': edu,
-            '工作经验': experience,
-            '公司名字': company,
-            '融资情况': financing,
-            '公司福利': temptation_str,
-            '招聘时间': release_time,
-            '简历反馈时间': feedback_time,
-        }
-        print(dit)
+    content = selector.css('#content::text') .getall()
+    pprint.pprint(content)
+    pass
 
 
 if __name__ == '__main__':
-    xiaoshuo_list()
+    xiaoshuo_mulu()
+    xiaoshuo_neirong(0)
     # for page in range(0, 10):
     #     main_thread = threading.Thread(target=main, args=(page,))
     #     main_thread.start()
--- pachong/utils/utils.py
+++ pachong/utils/utils.py
@@ -14,3 +14,16 @@ def change_title(title):
 
 
 fake_useragent 随机agent
+
+
+
+
+#(.*?)'.'匹配\n(换行符)以外的任何字符
+#(.*?)'*'前面的字符出现0次或以上
+#(.*?)'?'非贪婪模式
+ re.findall('<span class="bjh-p">(.*?)</span></p><p>',data )
+
+
+ #快速为 “：” 前后字符串加上单引号 需要find replace功能
+(.*?): (.*)
+'$1': '$2',
